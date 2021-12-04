e34fdec084759cdb0771e4c80fbbd9baa6985f7c
Carlos Rodriguez <carlosmvrodriguez9@gmail.com>
2021-12-04 14:39:02 -0300

    my first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <div className="App App-header">
+        <h1>Hola mundo</h1>
+        <Header/>
+
+      </div>
+    </>
+  );
+}
+
+export default App;
